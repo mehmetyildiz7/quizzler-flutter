0455730f6d9aa643a9767b5dc9bff22f44b84f40
Mehmet Yıldız <yildzmehmet7@gmail.com>
2019-08-16 11:26:16 +0300

    Temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +28,11 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<bool> answers = [
+    false, true, true
+  ];
+  List<Icon> scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,6 +69,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if(!correctAnswer){
+                  scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                }
+                else {
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
                 //The user picked true.
               },
             ),
@@ -80,11 +98,24 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = quizBrain.getQuestionAnswer();
+                if(!correctAnswer){
+                  scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                }
+                else {
+                  scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                }
+                setState(() {
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Wrap(
+          children: scoreKeeper,
+        )
       ],
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question{
+  String questionBody;
+  bool questionAnswer;
+
+  Question(String q, bool a){
+    questionBody = q;
+    questionAnswer = a;
+  }
+}
